6cc29eb81bb6a514416a1c1e3bd1a98444357be1
jules1468 <85050795+jules1468@users.noreply.github.com>
2024-06-08 08:05:41 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='image-detector-library',
+    version='0.1',
+    description='python library to find images in images',
+    author='jules1468',
+    packages=find_packages(),
+)
